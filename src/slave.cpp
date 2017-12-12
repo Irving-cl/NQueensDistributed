51db8a45a52133fb0be160c097249bc2b189f644
caoli <oos1111@sjtu.edu.cn>
2017-12-12 00:56:43 -0800

    fix wrongs

--- src/slave.cpp
+++ src/slave.cpp
@@ -1,4 +1,6 @@
 
+#include <sys/sysinfo.h>
+
 #include <iostream>
 #include <functional>
 
@@ -12,15 +14,16 @@ class session
 {
 public:
     session(boost::asio::io_service& ios, uint16_t port)
-      : acceptor_(ios, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+      : acceptor_(ios, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
+        socket_(ios)
     {
         do_accept();
     }
 
 private:
 
-    boost::asio::ip::tcp acceptor_;
-    boost::asio::ip::tcp socket_;
+    boost::asio::ip::tcp::acceptor acceptor_;
+    boost::asio::ip::tcp::socket socket_;
 
     void do_accept()
     {
@@ -29,6 +32,9 @@ private:
 
 int main()
 {
+    int core_count = get_nprocs();
+    std::cout << "core count: " << core_count << "\n";
+
     boost::asio::io_service ios;
 
     session s(ios, 10086);
